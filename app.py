06ad6c5376857db883277364309d1af0f6c9063d
UmutSahin0 <umutsahinn1@hotmail.com>
2024-03-30 23:16:54 +0300

    Added to fetch data from .xlsx by id

--- app.py
+++ app.py
@@ -3,14 +3,12 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Örnek bir veri dosyası yolu
 DATA_FILE_PATH = 'data.xlsx'
 
 def read_data(file_path):
     data = pd.read_excel(file_path)
     return data
 
-# data.xlsx dosyasını oku
 data = read_data(DATA_FILE_PATH)
 
 @app.route('/')
@@ -23,7 +21,6 @@ def print_name_with_name(name):
 
 @app.route('/id/<id>')
 def find_name_with_id(id):
-    # Veri dosyasından gelen veriyi kullanarak istenen işlemi yap
     filtered_data = data[data['id'] == int(id)]
     if not filtered_data.empty:
         return 'Name : {}'.format( filtered_data['ad'].iloc[0])
